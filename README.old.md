0743259367686baf80bab985c2bbd120bac5aaf1
unknown <gm.c.burzo@gmail.com>
2021-03-25 17:03:41 -0400

    1.1

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# project_oog
-First real web app: a video game review service
--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import "./App.css";
+
 function App() {
   return (
-    <div>
+    <div className="MAINPAGE">
+      <div className = "HEADER">
+
+      </div>
+      <div className="leftPanel">
+        leftPanel
+      </div>
+      <div className="Middle">
+        <div className = "adspace">
+          adspace
+        </div>
+        <div className = "reviewPanel">
+          reviewPanel
+        </div>
+      </div>
+      <div className="rightPanel">
+        rightPanel
+      </div>
     </div>
   );
 }
